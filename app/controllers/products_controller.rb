b1f9b4c43433f1c9f97eb6a707d6d9e0b6611ab5
huanuan <xieping7006@gmail.com>
2016-09-10 23:59:52 +0800

    modify product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :show, :create, :update, :edit, :destroy]
+  before_filter :authenticate_user!
   def index
     @products = Product.all
   end
@@ -8,38 +8,6 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update (product_params)
-      redirect_to products_path, notice:'Update Success'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to products_path, alert:'Product deleted'
-  end
 
   private
 
